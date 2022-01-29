398ae87636872220e09a2fc86248aec00d1f86d9
Jh0n4ttan <95657775+Jh0n4ttan@users.noreply.github.com>
2022-01-28 21:07:02 -0300

    Update Knight.cs

--- src/Entities/Knight.cs
+++ src/Entities/Knight.cs
@@ -1,7 +1,12 @@
 namespace Dotnet__Poo.src.Entities
 {
-    public class Knight
+    public class Knight : Hero
     {
-        
+        public Knight(String Name, int Level, string HeroType) :base(Name, Level, HeroType)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        } 
     }
 }
