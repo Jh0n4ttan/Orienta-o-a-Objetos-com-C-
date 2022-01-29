0cccc590231f79e8f90b148afe0f512097030c4d
Jh0n4ttan <95657775+Jh0n4ttan@users.noreply.github.com>
2022-01-28 21:07:25 -0300

    Update Wizard.cs

--- src/Entities/Wizard.cs
+++ src/Entities/Wizard.cs
@@ -1,7 +1,30 @@
 namespace Dotnet__Poo.src.Entities
 {
-    public class Wizard
+    public class Wizard : Hero
     {
         
+        public Wizard(String Name, int Level, string HeroType) :base(Name, Level, HeroType)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+
+
+        public override string Attack(){
+            return this.Name + "Lançou Magia ";
+        }
+
+
+        public string Attack(int Bonus)
+        {
+            if (Bonus > 6)
+            {
+                return this.Name + "Lançou uma magia super efetiva com bonus de " + Bonus;
+            }
+            else{
+                return this.Name + "Lançou uma magia com força fraca";
+            }
+            }
+        }
     }
-}
